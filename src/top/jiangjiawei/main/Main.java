f5481332041585968c22971aecfe391455c197e5
RealJiangLingJun <jiawei15214742755@163.com>
2023-02-25 10:23:31 +0800

    新增Main.java

---
+++ src/top/jiangjiawei/main/Main.java
@@ -0,0 +1,13 @@
+package top.jiangjiawei.main;
+
+/**
+ * @program: GitStudy
+ * @description: 主函数
+ * @author: LingJunJiang
+ * @create: 2023-02-25 10:22
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
