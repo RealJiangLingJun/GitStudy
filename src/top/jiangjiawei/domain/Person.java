8b2c43dfc84b252c8e017626e9a84792d6dcb444
RealJiangLingJun <jiawei15214742755@163.com>
2023-02-25 10:11:17 +0800

    项目首次提交，这里可以写提交信息

---
+++ src/top/jiangjiawei/domain/Person.java
@@ -0,0 +1,15 @@
+package top.jiangjiawei.domain;
+
+/**
+ * @program: GitStudy
+ * @description: Person
+ * @author: LingJunJiang
+ * @create: 2023-02-25 10:06
+ **/
+public class Person {
+    String name ;
+    int age;
+    String gender;
+
+    String info; // 这是修改的信息
+}
